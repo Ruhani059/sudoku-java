21aeb7bf792d129b1f15b6d6676bc6ee1318c1c6
ruhani-buet <1105059.rfr@ugrad.cse.buet.ac.bd>
2017-12-27 22:09:35 +0600

    Added test java files and test resources.

---
+++ src/test/java/com/ruhani/sudoku/SudokuSolverTest.java
@@ -0,0 +1,15 @@
+package com.ruhani.sudoku;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+
+public class SudokuSolverTest {
+
+    public static Logger logger = Logger.getLogger(SudokuSolverTest.class);
+
+    @Test
+    public void testClass() throws Exception {
+        logger.debug("testClass");
+    }
+}
